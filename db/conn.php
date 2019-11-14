e98f5866f4e8ab2668a256e929a79a8b03b42294
Denton19 <56175030+Denton19@users.noreply.github.com>
2019-11-14 12:51:51 -0500

    Update conn.php
    just added remote connections

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,17 @@
 <?php
-        $host = '127.0.0.1';
-        $db = 'attendance_db';
-        $user = 'root';
-        $pass = '';
+       //Developement Connection
+       // $host = '127.0.0.1';
+       // $db = 'attendance_db';
+       // $user = 'root';
+       // $pass = '';
+        //$charset = 'utf8mb4';
+
+
+        //Remote database connection
+        $host = 'remotemysql.com';
+        $db = '6fGKgbGjJQ';
+        $user = '6fGKgbGjJQ';
+        $pass = 'aeQpWCgrjC';
         $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
