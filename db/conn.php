e3b63d27516c1d1257079d10d94f8d4884f03f3f
Denton19 <56175030+Denton19@users.noreply.github.com>
2019-11-15 22:29:35 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,18 +1,18 @@
 <?php
        //Developement Connection
-        $host = '127.0.0.1';
-        $db = 'attendance_db';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
+        //$host = '127.0.0.1';
+        //$db = 'attendance_db';
+        //$user = 'root';
+        //$pass = '';
+        //$charset = 'utf8mb4';
 
 
         //Remote database connection
-        //$host = 'remotemysql.com';
-        //$db = '6fGKgbGjJQ';
-       // $user = '6fGKgbGjJQ';
-        //$pass = 'aeQpWCgrjC';
-        //$charset = 'utf8mb4';
+        $host = 'remotemysql.com';
+        $db = '6fGKgbGjJQ';
+        $user = '6fGKgbGjJQ';
+        $pass = 'aeQpWCgrjC';
+        $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
